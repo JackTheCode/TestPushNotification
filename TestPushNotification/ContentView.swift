9fa486ceb8d7d8e817ccd8617b136689004569f3
JackTheCode <phatnguyen0987654321@gmail.com>
2020-06-08 03:50:49 +0700

    Commit source
    Commit source

---
+++ TestPushNotification/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestPushNotification
+//
+//  Created by Phat Nguyen on 6/8/20.
+//  Copyright © 2020 Phat Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
